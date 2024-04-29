0df2123254dd4dde25d0b3de98f52d4a88a1af92
stg <a.sergiotorres@gmail.com>
2024-04-29 14:14:33 +0200

    FEAT: users migrations added.

--- src/database/migrations/1714390675301-roles.ts
+++ src/database/migrations/1714390675301-roles.ts
@@ -3,35 +3,37 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class Roles1714390675301 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        new Table({
-            name: "roles",
-            columns: [
-                {
-                    name: "id",
-                    type: "int",
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: "increment",
-                },
-                {
-                    name: "name",
-                    type: "varchar",
-                    length: "50",
-                },
-                {
-                    name: "created_at",
-                    type: "timestamp",
-                    default: "now()",
-                },
-                {
-                    name: "updated_at",
-                    type: "timestamp",
-                    default: "now()",
-                    onUpdate: "now()"
-                }
-            ],
-        }),
+        await queryRunner.createTable(
+            new Table({
+                name: "roles",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                        length: "50",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                        onUpdate: "now()"
+                    }
+                ],
+            }),
             true
+        )
 
     }
 
---
+++ src/database/migrations/1714392154222-users.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Users1714392154222 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "username",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: false
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: false,
+                        isUnique: true
+                    },
+                    {
+                        name: "password_hash",
+                        type: "varchar",
+                        length: "255",
+                        isNullable: false
+                    },
+                    {
+                        name: "role_id",
+                        type: "int",
+                        default: 2
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                        onUpdate: "now()"
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ["role_id"],
+                        referencedTableName: "roles",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+
+}
