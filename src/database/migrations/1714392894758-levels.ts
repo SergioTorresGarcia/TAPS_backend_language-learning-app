e7f7080a865d1a982a089dd4e471532ba1975d5f
stg <a.sergiotorres@gmail.com>
2024-04-29 14:17:34 +0200

    FEAT: levels migrations added.

---
+++ src/database/migrations/1714392894758-levels.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Levels1714392894758 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        new Table({
+            name: "levels",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: "increment",
+                },
+                {
+                    name: "name",
+                    type: "varchar",
+                    length: "200",
+                    isNullable: false
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "now()",
+                },
+                {
+                    name: "updated_at",
+                    type: "timestamp",
+                    default: "now()",
+                    onUpdate: "now()"
+                }
+            ]
+        })
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('levels');
+    }
+
+}
