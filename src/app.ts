04098a395d656487ac6700373a66807a7cba3805
stg <a.sergiotorres@gmail.com>
2024-04-29 12:06:14 +0200

    FEAT: FIRST COMMIT - configuration of express environment. Server running correctly.

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+// import cors from "cors";
+
+// import { roleRouter } from "./routes/roleRoutes";
+// import { authRouter } from "./routes/authRoutes";
+// import { userRouter } from "./routes/userRoutes";
+
+export const app = express();
+app.use(express.json());
+// app.use(cors());
+
+app.get("/alive", (req, res) => {
+    res.send("Server is alive")
+})
+app.get("/healthy", (req, res) => {
+    res.status(200).json(
+        {
+            "success": true,
+            "message": "Server is healthy"
+        }
+    )
+})
+
+
+// ENDPOINTS of the project:
+
+// app.use('/', roleRouter)
+
+// app.use('/', authRouter)
+
+// app.use('/', userRouter)
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+
+import { app } from "./app";
+import { Console } from "console";
+// import { AppDataSource } from "./database/db";
+
+const PORT = process.env.PORT || 4000
+
+const startServer = () => {
+    // AppDataSource.initialize()
+    //     .then(() => {
+    //         console.log("database connected")
+    //         app.listen(PORT, () => {
+    //             console.log(`Server is running on port: ${PORT}`);
+    //         })
+    //     })
+    //     .catch((error: any) => {
+    //         console.log(error)
+    //     })
+    app.listen(4500, () => console.log("Servidor levantado en 4500"))
+}
+
+startServer();
+// npm run dev <- command that runs the server
