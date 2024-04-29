31ee7e879a898076afc1e34790a4e2619d7f9916
stg <a.sergiotorres@gmail.com>
2024-04-29 14:23:49 +0200

    FEAT: words migrations added.

---
+++ src/database/migrations/1714393188539-words.ts
@@ -0,0 +1,85 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Words1714393188539 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "words",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "EN",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: false
+                    },
+                    {
+                        name: "JP",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: false,
+                        isUnique: true
+                    },
+                    {
+                        name: "romanji",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: false
+                    },
+                    ,
+                    {
+                        name: "image",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: false
+                    },
+                    {
+                        name: "level_id",
+                        type: "int"
+                    },
+                    {
+                        name: "challenge_id",
+                        type: "int"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                        onUpdate: "now()"
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ["level_id"],
+                        referencedTableName: "levels",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    },
+                    {
+                        columnNames: ["challenge_id"],
+                        referencedTableName: "challenges",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('words');
+    }
+
+}
